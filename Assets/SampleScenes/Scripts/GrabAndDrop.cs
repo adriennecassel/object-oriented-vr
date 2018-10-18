3c49e850fff374d8e1c5831490b6ff2bac2e40cc
adriennecassel <adriennecassel3@gmail.com>
2018-10-18 00:40:40 -0400

    begin project

---
+++ Assets/SampleScenes/Scripts/GrabAndDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabAndDrop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	GameObject GetMouseHoverObject(float range)
+	{
+		Vector3 position = gameObject.transform.position;
+		RaycastHit raycastHit;
+		Vector3 target = position + Camera.main.transform.forward * range;
+		
+		if (Physics.Linecast(position, target, out raycastHit))
+			return raycastHit.collider.gameObject;
+		return null;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(GetMouseHoverObject(5));
+		
+	}
+}
